f435dad89da6ef684cceee06b5e65c0e11a0c191
Aprille Perez <aprille.perez@002CLVDQ.local>
2020-04-24 13:26:14 -0700

    changing around some sections and effects

--- js/site.js
+++ js/site.js
@@ -1,13 +1,38 @@
 // Makes header section fade away
 $(window).scroll(function () {
-	$("header").css("opacity", 1 - $(window).scrollTop() / 500);
+	$("header").css("opacity", 1 - $(window).scrollTop() / 1000);
+	$("#intro").css("opacity", 1 - $(window).scrollTop() / 2500);
 });
+
+// Makes ATG expand on scroll
+$(function() {
+	var text = $(".hero-text");
+	$(window).scroll(function() {
+	  var scroll = $(window).scrollTop();
+  
+	  if (scroll >= 30) {
+		text.removeClass("hidden");
+	  } else {
+		text.addClass("hidden");
+	  }
+	});
+  });
+
+// WILL NEED FOR LATER!!!
+//   $(window).scroll(function() {
+// 	if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+// 	  $('#myDivId').hide();
+// 	}
+// 	else {
+// 	  $('#myDivId').show();
+// 	}
+//   });
 
 // Counter for stats
 var a = 0;
 $(window).scroll(function () {
-
-	var oTop = $('#stats').offset().top - window.innerHeight;
+//change back to stats if not working?
+	var oTop = $('#intro').offset().top - window.innerHeight;
 	if (a == 0 && $(window).scrollTop() > oTop) {
 		$('.counter').each(function () {
 			var $this = $(this),
