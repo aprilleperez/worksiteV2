0fb365b81067f194f510467430a43173341f485e
Aprille Perez <aprille.perez@002CLVDQ.local>
2020-04-27 16:28:20 -0700

    cleaning up code

--- js/site.js
+++ js/site.js
@@ -1,4 +1,4 @@
-// Makes header section fade away
+// Fade aways
 $(window).scroll(function () {
 	$("header").css("opacity", 1 - $(window).scrollTop() / 800);
 	$(".scroll-downs").css("opacity", 1 - $(window).scrollTop() / 150);
@@ -18,43 +18,3 @@ $(function() {
 	  }
 	});
   });
-
-// WILL NEED FOR LATER!!!
-//   $(window).scroll(function() {
-// 	if($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
-// 	  $('#myDivId').hide();
-// 	}
-// 	else {
-// 	  $('#myDivId').show();
-// 	}
-//   });
-
-// Counter for stats
-// var a = 0;
-// $(window).scroll(function () {
-// //change back to stats if not working?
-// 	var oTop = $('#intro').offset().top - window.innerHeight;
-// 	if (a == 0 && $(window).scrollTop() > oTop) {
-// 		$('.counter').each(function () {
-// 			var $this = $(this),
-// 				countTo = $this.attr('data-count');
-// 			$({
-// 				countNum: $this.text()
-// 			}).animate({
-// 				countNum: countTo
-// 			},
-// 				{
-// 					duration: 2000,
-// 					easing: 'swing',
-// 					step: function () {
-// 						$this.text(Math.floor(this.countNum));
-// 					},
-// 					complete: function () {
-// 						$this.text(this.countNum);
-// 						//alert('finished');
-// 					}
-// 				});
-// 		});
-// 		a = 1;
-// 	}
-// });
